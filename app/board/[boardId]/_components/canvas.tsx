fcaffd260d6640a033d6b8d7d7932925739b8127
RowdyKGZ <rowdykg@gmail.com>
2024-03-31 16:01:12 +0500

    room authentication

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useSelf } from "@/liveblocks.config";
+
 import { Info } from "./info";
 import { Toolbar } from "./toolbar";
 import { Participants } from "./participants";
@@ -9,6 +11,8 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const { name } = useSelf((me) => me.info);
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info />
